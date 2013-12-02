042fb2043f3171ef71dcd4cc9ec08ccbb28d019d
Julian Ceipek <julian.ceipek@gmail.com>
2013-12-02 00:12:19 -0500

    Better animated reveal

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -1,19 +1,31 @@
 $('document').ready(function () {
   var animateArrow = function (cb) {
-    $('.js-scroll-hint').animate({'margin-top': "1em"}, 100).animate({'margin-top': "0em"}, 100, cb);
+    $('.js-scroll-hint').animate({'top': "1em"}, 100).animate({'top': "0em"}, 100, cb);
   };
+
+  var have_scrolled = false;
 
   setTimeout(function () {
     var arrowAnim = setInterval(function () {
       animateArrow(function () {animateArrow(animateArrow)});
     }, 3000);
     $(window).scroll(function() {
+      have_scrolled = true;
       clearInterval(arrowAnim);
     });
-    $('#projects-header').click(function () {
-          $('html, body').animate({
-            scrollTop: $("#projects-header").offset().top
-          }, 500);
+
+    $('.js-scroll-button').hover(function () {
+      if (!have_scrolled)
+        $('.js-scroll-button').animate({'margin-top': '-2em'}, 300);
+    }, function () {
+      if (!have_scrolled)
+        $('.js-scroll-button').animate({'margin-top': '0'}, 300);
+    });
+    $('.js-scroll-button').click(function () {
+      $('.js-scroll-button').css('margin-top', '0');
+      $('html, body').animate({
+        scrollTop: $("#projects-header").offset().top
+      }, 500);
     });
   }, 1000);
 
