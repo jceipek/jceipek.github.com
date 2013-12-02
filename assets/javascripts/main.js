4033fc687b468476be3cc76dc182744842a6b8d6
Julian Ceipek <julian.ceipek@gmail.com>
2013-12-01 23:37:21 -0500

    Implemented click to scroll; bouncing arrow

---
+++ assets/javascripts/main.js
@@ -0,0 +1,19 @@
+$('document').ready(function () {
+  var animateArrow = function (cb) {
+    $('.js-scroll-hint').animate({'margin-top': "1em"}, 100).animate({'margin-top': "0em"}, 100, cb);
+  };
+
+  setTimeout(function () {
+    var arrowAnim = setInterval(function () {
+      animateArrow(function () {animateArrow(animateArrow)});
+    }, 3000);
+    $(window).scroll(function() {
+      clearInterval(arrowAnim);
+    });
+    $('#projects-header').click(function () {
+          $('html, body').animate({
+            scrollTop: $("#projects-header").offset().top
+          }, 500);
+    });
+  }, 1000);
+});
